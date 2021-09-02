dae7bc676adc5613ee80cb5580cc090b1ad9d01c
Panji Setiawan <stickearn@StickEarns-MacBook-Air.local>
2021-09-02 23:54:18 +0700

    update

--- get_eval.sql
+++ get_eval.sql
@@ -1,13 +1,14 @@
 select id,odometer,number,startDate from evaEvaluation
 where deleted_at is null
-and date(uploadedAt) = date(now())
+and date(DATE_ADD(uploadedAt, INTERVAL 7 HOUR)) = date(DATE_ADD(now(), INTERVAL 7 HOUR))
 and verifiedAt is null
 and number > 0
-and systemProcessedAt is null
+#and systemProcessedAt is null
 and odometer > 0
+and id = 3535278  #3538191    #3537045 #3535278
 
 and (evaEvaluation.cpnAssign_id, evaEvaluation.number) not in (
-	select cpnAssign_id, max(number) from evaEvaluation
+        select cpnAssign_id, max(number) from evaEvaluation
     where deleted_at is null
     group by 1
 )
